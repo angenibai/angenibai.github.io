49e4036131fbc3bc40b98983624d2030382c5cf3
Angeni Bai <angenibai@gmail.com>
2021-07-24 23:39:01 +1000

    dark mode somewhat converted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,55 @@
-function switch_dark_mode_icon(darkmode, id) {
-    darkmode.toggle();
-    render_dark_mode_icon(darkmode, id);
+function switch_dark_mode_icon(settings, id) {
+    const body = document.getElementsByTagName('body')[0];
+    const root = document.documentElement;
+    if (localStorage.darkModeCustom === 'dark') {
+        localStorage.darkModeCustom = 'light';
+        root.setAttribute('dark-mode', 'light');
+    } else {
+        localStorage.darkModeCustom = 'dark';
+        root.setAttribute('dark-mode', 'dark');
+    }
+    /*
+    const enabled = localStorage.getItem('dark-mode');
+    if (enabled === 'true') {
+        DarkReader.disable();
+        localStorage.setItem('dark-mode', 'false')
+    } else if (enabled === 'false') {
+        DarkReader.enable(settings);
+        localStorage.setItem('dark-mode', 'true');
+    }
+    if (DarkReader.isEnabled()) {
+        DarkReader.disable();
+    } else {
+        DarkReader.enable(settings);
+    }
+    */
+    render_dark_mode_icon(id);
 }
 
-function render_dark_mode_icon(darkmode, id) {
-    var x = document.getElementsByTagName("img");
-    var i;
-
-    console.log(x)
-    if (darkmode.isActivated()) {
+function render_dark_mode_icon(id) {
+    const body = document.getElementsByTagName("body")[0];
+    const root = document.documentElement;
+    if (localStorage.darkModeCustom === 'dark') {
         document.getElementById(id).classList.remove('fa-sun');
-        document.getElementById(id).classList.add('fa-moon');
-        for (i = 0; i < x.length; i++) {
-            x[i].style["mix-blend-mode"] = "difference";
-        }
-
+        document.getElementById(id).classList.add('fa-moon'); 
+        root.setAttribute('dark-mode', 'dark');
     } else {
         document.getElementById(id).classList.remove('fa-moon');
-        document.getElementById(id).classList.add('fa-sun');
-        for (i = 0; i < x.length; i++) {
-            x[i].style["mix-blend-mode"] = "normal";
-        }
+        document.getElementById(id).classList.add('fa-sun'); 
+        root.setAttribute('dark-mode', 'light');
     }
+
+    /*var x = document.getElementsByTagName("img");
+    const enabled = localStorage.getItem('dark-mode');
+
+    console.log(x)
+    if (enabled === 'true') {
+        document.getElementById(id).classList.remove('fa-sun');
+        document.getElementById(id).classList.add('fa-moon');
+    } else if (enabled === 'false') {
+        document.getElementById(id).classList.remove('fa-moon');
+        document.getElementById(id).classList.add('fa-sun');
+    }
+    */
 }
 
