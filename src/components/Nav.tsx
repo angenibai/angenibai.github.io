ac5f7142435cceda496818a8283668220a4c739e
Angeni Bai <angenibai@gmail.com>
2023-07-21 01:45:53 +1000

    Arranging nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,18 +4,18 @@ import styles from "@/styles/components/Nav.module.css";
 
 const Nav = () => {
   return (
-    <header>
-      <Link href="/" className={styles.websiteTitle}>
-        angeni bai
-      </Link>
-      <nav>
-        <div>
+    <header className={styles.header}>
+      <div className={styles.websiteTitle}>
+        <Link href="/">angeni bai</Link>
+      </div>
+      <nav className={styles.nav}>
+        <div className={styles.navItem}>
           <Link href="/">home</Link>
         </div>
-        <div>
+        <div className={styles.navItem}>
           <Link href="/projects">projects</Link>
         </div>
-        <div>
+        <div className={styles.navItem}>
           <Link href="/posts">posts</Link>
         </div>
       </nav>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
