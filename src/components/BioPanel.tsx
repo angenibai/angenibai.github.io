9f91e476bc38f07cab0eedfd6b688612dc387e2d
Angeni Bai <angenibai@gmail.com>
2023-07-21 10:21:29 +1000

    Bio panel

---
+++ src/components/BioPanel.tsx
@@ -0,0 +1,99 @@
+import styles from "@/styles/components/BioPanel.module.css";
+import Image from "next/image";
+
+interface BioPanelEntry {
+  label: string;
+  value: string;
+}
+
+interface BioPanelSection {
+  title: string | null;
+  entries: BioPanelEntry[];
+}
+
+interface BioPanelContent {
+  heading: string;
+  img: string | null;
+  sections: BioPanelSection[];
+}
+
+const content: BioPanelContent = {
+  heading: "angeni",
+  img: null,
+  sections: [
+    {
+      title: null,
+      entries: [
+        {
+          label: "aka",
+          value: "genie 🧞",
+        },
+      ],
+    },
+    {
+      title: "fun facts",
+      entries: [
+        {
+          label: "likes",
+          value: "kanzi apples",
+        },
+        {
+          label: "dislikes",
+          value: "hardback books",
+        },
+      ],
+    },
+  ],
+};
+
+const BioPanel = () => {
+  return (
+    <div className={styles.bioPanel}>
+      <div className={styles.bioPanelContent}>
+        <div
+          className={`${styles.bioPanelHeader} ${styles.bioPanelTitleHeader}`}
+        >
+          <h4>{content.heading}</h4>
+        </div>
+        <div className={styles.bioPanelImgFrame}>
+          {content.img ? (
+            <Image src={content.img} alt={content.heading} placeholder="blur" />
+          ) : (
+            <div className={styles.bioPanelImgPlaceholder}>
+              <p>{"in progress :)"}</p>
+            </div>
+          )}
+        </div>
+        {content.sections.map((section) => {
+          return (
+            <div className={styles.bioPanelSection}>
+              {section.title && (
+                <div
+                  className={`${styles.bioPanelHeader} ${styles.bioPanelSectionHeader}`}
+                >
+                  <h5>{section.title}</h5>
+                </div>
+              )}
+              <div className={styles.bioPanelSectionContent}>
+                {section.entries.map((entry) => {
+                  return (
+                    <div className={styles.bioPanelSectionEntry}>
+                      <div className={styles.bioPanelSectionEntryLabel}>
+                        <p>{entry.label}</p>
+                      </div>
+                      <div className={styles.bioPanelSectionEntryValue}>
+                        <p>{entry.value}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BioPanel;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import BioPanel from "@/components/BioPanel";
 import Head from "next/head";
+
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -9,9 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={``}>
-        <h1>home</h1>
-      </main>
+      <div className={styles.homePage}>
+        <BioPanel />
+      </div>
     </>
   );
 }
