5ada0c89d1b6ca922ba93e4a7212c700c24eb39d
Angeni Bai <angenibai@gmail.com>
2023-08-18 22:22:17 +1000

    Fix line spacing

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,49 +1,9 @@
 import styles from "@/styles/components/ProjectGrid.module.css";
 import ProjectTile from "@/components/ProjectTile";
 import { ProjectContent } from "@/types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { NextSeo } from "next-seo";
 import { getAllProjects } from "@/lib/api";
-
-// const projectsHardcoded: ProjectContent[] = [
-//   {
-//     name: "Who Guessed",
-//     shortDescription: "Play Guess Who online",
-//     longDescription: (
-//       <>
-//         <p>Generate a Guess Who board to play with friends!</p>
-//         <p>Currently supports playing with pre-loaded character decks</p>
-//       </>
-//     ),
-//     tags: ["web-dev"],
-//   },
-//   {
-//     name: "This website",
-//     shortDescription: "Version 2.0",
-//     longDescription: (
-//       <>
-//         <p>Stack:</p>
-//         <ul>
-//           <li>Next.js</li>
-//         </ul>
-//       </>
-//     ),
-//     tags: ["web-dev"],
-//   },
-//   {
-//     name: "Mario-dle",
-//   },
-//   {
-//     name: "Can I Go Outside?",
-//     shortDescription:
-//       "alskdf aslcmasldk fwjerfio sdklvcn askldc asodkjc ioasd cnasldkv alkf ioasajkdfvn djkasv ioe fiowerf weomfa sdklcalskdcmioweacmwioeacmio wemcwioae klasdcm askld cklasdjc io weajc wel calksdcnm klasdc",
-//   },
-//   {
-//     name: "lorem ipsum the quick brown fox jumped over  the lazy lazy lazy dog and it keeeeeeeeps goinnggagfdgnjkdfgn asjkldf asldfj askldfjasdklf weiofjwekjf",
-//     shortDescription:
-//       "alskdf aslcmasldk fwjerfio sdklvcn askldc asodkjc ioasd cnasldkv alkf ioasajkdfvn djkasv ioe fiowerf weomfa sdklcalskdcmioweacmwioeacmio wemcwioae klasdcm askld cklasdjc io weajc wel calksdcnm klasdc",
-//   },
-// ];
 
 interface ProjectsProps {
   projects: ProjectContent[];
